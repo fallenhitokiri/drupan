f49d8bd1c57c7f227121e5e05fe3c730c3496128
Timo Zimmermann <timo@screamingatmyscreen.com>
2012-10-19 23:51:12 +0200

    move argparse, change core.py
    move argparse and change the way core is setup to allow easily
    importing the engine

--- drupan/core.py
+++ drupan/core.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import argparse
 import sys
 
 from drupan.site import Site
@@ -11,28 +10,15 @@ from drupan.initialize import bootstrap
 
 class Drupan(object):
     """drupan engine - everything happens here"""
-    def __init__(self):
-        """parse arguments and create site object"""
-        parser = argparse.ArgumentParser(description='drupan')
-        parser.add_argument('path', help='path to configuration file')
-        parser.add_argument('--nodeploy', help='do not deploy',
-                             action='store_true', default=False)
-        parser.add_argument('--serve', help='serve site',
-                             action='store_true', default=False)
-        parser.add_argument('--init', help="create new site at path",
-                             action='store_true', default=False)
-        args = parser.parse_args()
-
-        if not vars(args)['init']:
-            self.site = Site()
-            self.site.path = vars(args)['path']
-            self.no_deployment = vars(args)['nodeploy']
-            self.serve = vars(args)['serve']
-        else:
-            self.initialize(vars(args)['path'])
+    def setup(self, path, nodeploy, serve):
+        """setup engine"""
+        self.site = Site()
+        self.site.path = path
+        self.no_deployment = nodeploy
+        self.serve = serve
 
     def run(self):
-        """run denbo
+        """run drupan
         - setup site object
         - load plugins
         - run plugins
