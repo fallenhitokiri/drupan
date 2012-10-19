ad24c7131166b91172dfdfdd1d91929d7be751f7
Timo Zimmermann <timo@screamingatmyscreen.com>
2012-10-19 23:25:18 +0200

    add tests
    tests for tags, highlight and blank (overall generation) plugin

---
+++ tests/test_plugin_generation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+from drupan.plugins import blank
+
+
+class Config(object):
+    def __init__(self):
+        self.url = "http://foo.tld/"
+        self.layouts = {
+            'index': ["", "_index.html"]
+        }
+
+    def options_for_key(self, key):
+        opt = {
+            'index': ['Index', 'False']
+        }
+        return opt
+
+
+class So(object):
+    """small site object"""
+    def __init__(self):
+        cfg = Config()
+        self.content = []
+        self.config = cfg
+
+
+class ValidGenerationTest(unittest.TestCase):
+    def test_highlight(self):
+        site = So()
+        plugin = blank.Feature(site)
+        plugin.run()
+        title = site.content[0].meta['title']
+        layout = site.content[0].meta['layout']
+        menu = site.content[0].menu
+        self.assertEqual(title, 'Index')
+        self.assertEqual(layout, 'index')
+        self.assertEqual(menu, 'False')
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_plugin_tag.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+from drupan.plugins import tags
+
+
+class Obj(object):
+    """small content object"""
+    def __init__(self):
+        self.meta = {
+            'tags': ['tag1', 'tag2']
+        }
+
+
+class Config(object):
+    def __init__(self):
+        self.url = "http://foo.tld/"
+        self.layouts = {
+            'tags': ["tag/$title/", "_tag.html"]
+        }
+
+
+class So(object):
+    """small site object"""
+    def __init__(self):
+        co = Obj()
+        self.content = [co]
+        cfg = Config()
+        self.config = cfg
+
+
+class ValidTagTest(unittest.TestCase):
+    def test_markup(self):
+        site = So()
+        plugin = tags.Feature(site)
+        plugin.run()
+        self.assertTrue("tag1" in plugin.tags)
+        self.assertTrue("tag2" in plugin.tags)
+        self.assertEqual(site.content[1].url, "http://foo.tld/tag/tag1/")
+        self.assertEqual(site.content[2].url, "http://foo.tld/tag/tag2/")
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
