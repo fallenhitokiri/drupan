6bd18f6083f6812efa6346d38f98eda3fadd2478
Timo Zimmermann <timo@screamingatmyscreen.com>
2012-10-22 16:59:02 +0200

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup
 
 setup(
@@ -24,4 +26,26 @@ setup(
               'drupan = drupan.main:cmd'
           ]
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: BSD :: FreeBSD',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Code Generators',
+    ],
 )
