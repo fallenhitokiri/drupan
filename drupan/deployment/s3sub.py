3281287e5e16b96df3d0b32eb3b1aaa4810157c1
Timo Zimmermann <timo@screamingatmyscreen.com>
2014-05-19 22:24:10 +0200

    initial version of s3sub deployment

---
+++ drupan/deployment/s3sub.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    drupan.deployment.s3sub
+
+    Deploy your site to AWS S3. The AWS cli tool is called via subprocess.
+"""
+
+import subprocess
+
+
+class Deploy(object):
+    def __init__(self, site, config):
+        """
+        Arguments:
+            site: generated site
+            config: config for this site
+        """
+        self.site = site
+        self.config = config
+
+        self.path = config.get_option("s3sub", "path")
+
+    def run(self):
+        """run the deployment process"""
+        s3path = "s3://{0}".format(self.bucket)
+        proc = subprocess.Popen(
+            ["aws", "s3", "--acl", "public-read", "sync", ".", s3path],
+            cwd=self.path
+        )
+        proc.communicate()
