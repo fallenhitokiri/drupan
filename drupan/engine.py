e3fe29c502c89f3203468fc66ba8183ed3648e25
Timo Zimmermann <timo@screamingatmyscreen.com>
2018-06-17 23:08:05 +0200

    remove reader / writer requirement

--- drupan/engine.py
+++ drupan/engine.py
@@ -91,11 +91,13 @@ class Engine(object):
 
     def run(self):
         """run the site generation process"""
-        self.reader.run()
+        if self.reader:
+            self.reader.run()
         for plugin in self.plugins:
             plugin.run()
         self.renderer.run()
-        self.writer.run()
+        if self.writer:
+            self.writer.run()
 
     def serve(self):
         """serve the generated site"""
